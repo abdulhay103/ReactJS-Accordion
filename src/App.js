5056f8e741f4fcb3e93adabb5ab474680b0e6fee
Abdul Hay <mdabdulhay95@gmail.com>
2021-03-23 00:05:43 +0600

    Final Upload

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ function App() {
     <main>
       <div className="container">
         <h3>Header Notification displayer</h3>
+
         <section className="info">
           {questions.map((question) => {
-            console.log(question);
+            // console.log(question);
             return <SingleQuestion key={question.id} {...question} />;
           })}
+          <p>A part of CandyCrab@2021</p>
         </section>
       </div>
     </main>
